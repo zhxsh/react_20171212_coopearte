b7675e01723a542f96dda3931de62af70f33ee4f
zhxsh <972678896@qq.com>
2017-12-12 11:36:13 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,4 @@
+var react = require('react');
+import './index.css';
+
+console.log(react.version)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		name: './app/index.js'
+	},
+	output: {
+		path: path.resolve(__dirname,'./dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{ 
+				test: /\.js|jsx/, 
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react','es2015']
+				}
+			},
+			{ 
+				test: /\.css/,
+				loader: ['style-loader','css-loader']
+			}
+		]
+	}
+}
